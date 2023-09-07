f1a771b51853b1940a51bfe8a1dd10af35e2e3ac
irfan2627 <130214025+irfan2627@users.noreply.github.com>
2023-09-07 16:17:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+      
+function toggleMenu1(){
+
+    let wrapMenu = document.getElementById("wrapMenu");
+    wrapMenu.classList.toggle("open-menu");
+
+    let wrapHead = document.getElementById("wrapHead");
+    wrapHead.classList.toggle("close-head");
+
+}
+
+function toggleMenu2(){
+
+    let wrapLoc = document.getElementById("wrapLoc");
+    wrapLoc.classList.toggle("openLocWrap");
+
+}
+
+function toggleMenu3(){
+
+    let wrapText = document.getElementById("wrapText");
+    wrapText.classList.toggle("openText");
+} 
+
+
+
